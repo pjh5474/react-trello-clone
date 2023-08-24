ae0ce1bf9e8c9f7598e19a6a0bead74452081e83
pjh5474 <warwarsn@gmail.com>
2023-08-24 16:40:56 +0900

    Drap & Drop Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+const GlobalStyle = createGlobalStyle`
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+* {
+	box-sizing: border-box;
+}
+body {
+	font-family: 'Source Sans Pro', sans-serif;
+	background-color: ${(props) => props.theme.bgColor};
+	color: ${(props) => props.theme.textColor};
+}
+a {
+	text-decoration: none;
+  color: inherit;
+}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const onDragEnd = () => {};
+	return (
+		<>
+			<HelmetProvider>
+				<Helmet>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap"
+						rel="stylesheet"
+					/>
+				</Helmet>
+			</HelmetProvider>
+			<GlobalStyle />
+			<DragDropContext onDragEnd={onDragEnd}>
+				<div>
+					<Droppable droppableId="one">
+						{(magic) => (
+							<ul
+								ref={magic.innerRef}
+								{...magic.droppableProps}
+							>
+								<Draggable
+									draggableId="first"
+									index={0}
+								>
+									{(magic) => (
+										<li
+											ref={magic.innerRef}
+											{...magic.draggableProps}
+											{...magic.dragHandleProps}
+										>
+											<span {...magic.dragHandleProps}>(*/ω＼*)</span>
+											<span>First</span>
+										</li>
+									)}
+								</Draggable>
+								<Draggable
+									draggableId="second"
+									index={1}
+								>
+									{(magic) => (
+										<li
+											ref={magic.innerRef}
+											{...magic.draggableProps}
+										>
+											<span>Second</span>
+											<span {...magic.dragHandleProps}>
+												○( ＾皿＾)っ Drag me!
+											</span>
+										</li>
+									)}
+								</Draggable>
+								<Draggable
+									draggableId="third"
+									index={2}
+								>
+									{(magic) => (
+										<li
+											ref={magic.innerRef}
+											{...magic.draggableProps}
+											{...magic.dragHandleProps}
+										>
+											Third
+										</li>
+									)}
+								</Draggable>
+							</ul>
+						)}
+					</Droppable>
+				</div>
+			</DragDropContext>
+		</>
+	);
 }
 
 export default App;
